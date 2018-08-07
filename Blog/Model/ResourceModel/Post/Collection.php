b5638dd096022b9f875068e14d3a0f3ed38b1587
Toan Thang <vietnga1910@gmail.com>
2018-08-07 10:58:55 +0700

    Update di.xml + filter by store view but not working ~~

--- Blog/Model/ResourceModel/Post/Collection.php
+++ Blog/Model/ResourceModel/Post/Collection.php
@@ -5,9 +5,21 @@ use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 class Collection extends AbstractCollection
 {
     protected $_idFieldName = 'post_id';
+    protected $_previewFlag;
+
     protected function _construct()
     {
         $this->_init(ThangDz\Blog\Model\Post::class, ThangDz\Blog\Model\ResourceModel\Post::class);
     }
 
+    protected function _afterLoad()  {
+        $this->performAfterLoadBlog('blog_post_store', 'post_id');
+        $this->_previewFlag = false;
+
+        return parent::_afterLoad();
+    }
+
+    protected function _renderFiltersBefore() {
+        $this->joinStoreRelationTable('blog_post_store', 'post_id');
+    }
 }
